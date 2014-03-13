b47706752f9f4e9b751077595709b9e34227a7ab
Felipe Calderon Barragan <felipeskabarragan@gmail.com>
2014-03-13 18:16:58 -0500

    fix

--- Apps/playstore-api/playStore.js
+++ Apps/playstore-api/playStore.js
@@ -27,7 +27,7 @@ var playStore = (function() {
 						var thumbnails = extractThumbs($);
 						var additionalInfo = extractAddtitionalInfo($);
 						var developer = $('div[itemprop="author"]').children('a').children('span').text();
-						var category = $('.document-subtitle-category').children('span').text();
+						var category = $('.document-subtitle category').children('span').text();
 						console.log(category);
 
 						// get permissions for app
